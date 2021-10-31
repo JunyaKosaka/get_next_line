74d742f210f2653e9a940729ffbd40c7c20c7bf5
Junya Kosaka <kosakajunya@gmail.com>
2021-10-31 18:42:56 +0900

    modify

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 14:05:15 by jkosaka           #+#    #+#             */
-/*   Updated: 2021/10/29 22:17:52 by jkosaka          ###   ########.fr       */
+/*   Updated: 2021/10/31 18:41:41 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -25,122 +24,97 @@ char	*ft_strchr(const char *s, int c)
 static char	*get_one_line(char **save, size_t len)
 {
 	char	*ret;
+	char	*new;
 
-	if (**save == '\0')
+	if (!len)
+	{
+		free(*save);
+		return (NULL);
+	}
+	if (!(*save))
 		return (NULL);
 	ret = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ret)
 		return (NULL);
 	ft_strlcpy(ret, *save, len + 1);
-	*save += len + 1;
+	new = ft_strdup((*save) + len);
+	free(*save);
+	*save = new;
 	return (ret);
 }
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	get_len(const char *s, char c)
 {
-	size_t	ret;
-	size_t	dst_len;
-	size_t	src_len;
-
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	ret = dst_len + src_len;
-	if (dstsize < dst_len + 1)
-		return (dstsize + src_len);
-	ft_strlcpy(dst + dst_len, src, dstsize - dst_len);
-	return (ret);
+	return (ft_strchr(s, c) - s + 1);
 }
 
-void *word_join(char **s1, char *s2)
+void	*free_all(char *s1, char *s2)
 {
-	char	*new;
-	size_t	s1_len;
-	size_t	s2_len;
-	size_t	size;
-
-	if (!(*s1))
-		*s1 = "";
-	s1_len = ft_strlen(*s1);
-	s2_len = ft_strlen(s2);
-	size = s1_len + s2_len;
-	new = (char *)malloc(sizeof(char) * (size + 1));
-	if (!new)
-	{
-		free(*s1);
-		free(s2);
-		return (NULL);
-	}
-	ft_strlcpy(new, *s1, s1_len + 1);
-	ft_strlcpy(new + s1_len, s2, size + 1);
+	free(s1);
 	free(s2);
-	*s1 = new;
-	return (*s1);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*save[FOPEN_MAX] = {NULL};
+	static char	*save[FD_MAX];  // = {NULL};
 	char		*buff;
 	int			read_bytes;
 
-	if (fd < 0 || FOPEN_MAX <= fd|| BUFFER_SIZE <= 0)
+	if (fd < 0 || FD_MAX <= fd || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (save[fd] && ft_strchr(save[fd], '\n'))
-		return (get_one_line(&save[fd], ft_strchr(save[fd], '\n') - save[fd] + 1));
-	while (1)
+	buff = (char *)malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	if (!save[fd])
+		save[fd] = ft_strdup("");
+	read_bytes = 1;
+	while (read_bytes && !ft_strchr(save[fd], '\n'))
 	{
-		buff = (char *)malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1));
-		if (!buff)
-			return (NULL);
 		read_bytes = read(fd, buff, BUFFER_SIZE);
-		if (read_bytes <= 0)
-		{
-			free(buff);
-			return (get_one_line(&(save[fd]), ft_strlen(save[fd])));
-		}
+		if (read_bytes == -1)
+			return (free_all(buff, save[fd]));
 		buff[read_bytes] = '\0';
-		word_join(&save[fd], buff);
-		// free(buff);
-		if (save[fd] && ft_strchr(save[fd], '\n'))
-			return (get_one_line(&save[fd], ft_strchr(save[fd], '\n') - save[fd] + 1));
+		save[fd] = join_words(save[fd], buff);
 	}
-	return (NULL);
+	free(buff);
+	if (!read_bytes)
+		return (get_one_line(&save[fd], ft_strlen(save[fd])));
+	return (get_one_line(&save[fd], get_len(save[fd], '\n')));
 }
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
+// #include <fcntl.h>
+// #include <sys/types.h>
+// #include <sys/uio.h>
+// #include <unistd.h>
+// #include <errno.h>
+// #include <string.h>
+// #include <stdlib.h>
 
-int	main(void)
-{
-    int 	fd;
-	// int		fd02;
-    char 	*buff;
-    // char 	*buff02;
+// int	main(void)
+// {
+//     int 	fd;
+// 	// int		fd02;
+//     char 	*buff;
+//     // char 	*buff02;
 
-    if ((fd = open("sample1.txt", O_RDONLY)) == -1)
-    {
-		printf("fopen error(%s)\n", strerror(errno));
-        return (0);
-    }
-	// fd = 0;
-    // if ((fd02 = open("sample5.txt", O_RDONLY)) == -1)
-    // {
-	// 	printf("fopen error(%s)\n", strerror(errno));
-    //     return (0);
-    // }
-	printf("%d\n", FOPEN_MAX);
-	while (1)
-    {
-		buff = get_next_line(fd);
-		printf("answer:%s\n", buff);
-		// printf("answer:%s\n", buff02);
-		break;
-    }
-    close(fd);
-	// system("leaks a.out"); // メモリリークチェック
-    return (0);
-}
+//     if ((fd = open("sample6.txt", O_RDONLY)) == -1)
+//     {
+// 		printf("fopen error(%s)\n", strerror(errno));
+//         return (0);
+//     }
+// 	// fd = 0;
+//     // if ((fd02 = open("sample5.txt", O_RDONLY)) == -1)
+//     // {
+// 	// 	printf("fopen error(%s)\n", strerror(errno));
+//     //     return (0);
+//     // }
+// 	while ((buff = get_next_line(fd)))
+//     {
+// 		printf("answer:%s", buff);
+// 		// printf("answer:%s\n", buff02);
+//     }
+//     close(fd);
+// 	// system("leaks a.out"); // メモリリークチェック
+//     return (0);
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 14:09:08 by jkosaka           #+#    #+#             */
-/*   Updated: 2021/10/28 16:44:34 by jkosaka          ###   ########.fr       */
+/*   Updated: 2021/10/31 18:42:14 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,31 @@ char	*ft_strdup(const char *s1)
 	char	*ret;
 	size_t	len;
 
+	if (!s1)
+		return (NULL);
 	len = ft_strlen(s1);
 	ret = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ret)
 		return (NULL);
 	ft_strlcpy(ret, s1, len + 1);
+	return (ret);
+}
+
+char	*join_words(char const *s1, char const *s2)
+{
+	char	*ret;
+	size_t	size;
+
+	if (!s1 || !s2)
+		return (NULL);
+	size = ft_strlen(s1) + ft_strlen(s2);
+	ret = (char *)malloc(sizeof(char) * (size + 1));
+	if (!ret)
+		return (NULL);
+	ft_strlcpy(ret, s1, ft_strlen(s1) + 1);
+	ft_strlcpy(ret + ft_strlen(s1), s2, size + 1);
+	// printf("++%s%s%s--\n", ret, s1, s2);
+	if (s1 != s2 && s1 != ret)
+		free((char *)s1);
 	return (ret);
 }
