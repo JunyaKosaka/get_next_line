fc87c4f4b74b6699a2d1e326c977c8befa06432e
Junya Kosaka <kosakajunya@gmail.com>
2021-11-02 21:20:04 +0900

    bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,11 +6,11 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 14:05:15 by jkosaka           #+#    #+#             */
-/*   Updated: 2021/11/02 21:16:31 by jkosaka          ###   ########.fr       */
+/*   Updated: 2021/11/02 21:17:46 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_strchr(const char *s, int c)
 {
